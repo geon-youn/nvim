f8248fb4793b4268847a58b054e4b2c753f3795e
Geon Youn <geon.youn@outlook.com>
2024-02-27 12:30:25 -0500

    update obsidian.nvim config and vim sets

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,24 +4,24 @@ lsp.preset("recommended")
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-	ensure_installed = {'tsserver', 'rust_analyzer', 'lua_ls'},
-	handlers = {lsp.default_setup}
+  ensure_installed = { 'tsserver', 'rust_analyzer', 'lua_ls' },
+  handlers = { lsp.default_setup }
 })
 
 -- Fix Undefined global 'vim'
 lsp.configure('lua_ls', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      }
     }
+  }
 })
 
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
@@ -32,20 +32,20 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 cmp_mappings['<Tab>'] = nil
 cmp_mappings['<S-Tab>'] = nil
 
-cmp.setup({mapping = cmp_mappings})
+cmp.setup({ mapping = cmp_mappings })
 
 lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
+  suggest_lsp_servers = false,
+  sign_icons = {
+    error = 'E',
+    warn = 'W',
+    hint = 'H',
+    info = 'I'
+  }
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -62,6 +62,5 @@ end)
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true
+  virtual_text = true
 })
-
--- lua/geon/packer.lua
+++ lua/geon/packer.lua
@@ -4,56 +4,56 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
-        -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    -- or                            , branch = '0.1.x',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+
+  use({ 'rose-pine/neovim', as = 'rose-pine' })
+
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+
+  use('nvim-treesitter/nvim-treesitter-context')
+
+  use('nvim-treesitter/playground')
+
+  use('theprimeagen/harpoon')
+
+  use('mbbill/undotree')
+
+  use('tpope/vim-fugitive')
+
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    requires = {
+      --- Uncomment these if you want to manage the language servers from neovim
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'L3MON4D3/LuaSnip' },
     }
+  }
 
-    use({ 'rose-pine/neovim', as = 'rose-pine' })
+  use('theprimeagen/vim-be-good')
 
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-
-    use('nvim-treesitter/nvim-treesitter-context')
-
-    use('nvim-treesitter/playground')
-
-    use('theprimeagen/harpoon')
-
-    use('mbbill/undotree')
-
-    use('tpope/vim-fugitive')
-
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        requires = {
-            --- Uncomment these if you want to manage the language servers from neovim
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
-
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'L3MON4D3/LuaSnip' },
-        }
-    }
-
-    use('theprimeagen/vim-be-good')
-
-    use({
-        "epwalsh/obsidian.nvim",
-        tag = "*",                             -- recommended, use latest release instead of latest commit
-        requires = {
-            "nvim-lua/plenary.nvim",           -- required
-            'hrsh7th/nvim-cmp',                -- autocomplete
-            'nvim-telescope/telescope.nvim',   -- search functionality
-            'nvim-treesitter/nvim-treesitter', -- syntax highlight
-        },
-    })
+  use({
+    "epwalsh/obsidian.nvim",
+    tag = "*",                                 -- recommended, use latest release instead of latest commit
+    requires = {
+      "nvim-lua/plenary.nvim",                 -- required
+      'hrsh7th/nvim-cmp',                      -- autocomplete
+      'nvim-telescope/telescope.nvim',         -- search functionality
+      'nvim-treesitter/nvim-treesitter',       -- syntax highlight
+    },
+  })
 end)
